65657fdd808ad93648e62d01b25b7c6e1197526b
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-03-29 17:38:34 +0900

    change file directory for hero images

--- src/components/home/cansat.tsx
+++ src/components/home/cansat.tsx
@@ -1,23 +1,21 @@
 "use client";
 
-import Image from "next/image";
 import { LearnMore } from "../button";
 import { HeroTitleImage } from "./heroTitle";
 import Link from "next/link";
 import HeroImage from "./heroImage";
-import { motion } from "framer-motion";
 
 export default function Cansat() {
   return (
     <section>
       <div className="relative w-full h-screen z-0">
         <div className="absolute z-50 top-[50%] md:ml-24 ml-6 w-[50%]">
-          <HeroTitleImage src="/CansatLogo.png" link="/cansat" />
+          <HeroTitleImage src="/cansat/CansatLogo.png" link="/cansat" />
           <Link href="/cansat">
             <LearnMore />
           </Link>
         </div>
-        <HeroImage src="/CansatHeroImage3.jpg" />
+        <HeroImage src="/cansat/CansatHeroImage3.jpg" />
       </div>
     </section>
   );
