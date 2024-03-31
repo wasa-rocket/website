f90e3617e5e69c9c6b9017c4a6fbba5355d031f3
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-03-31 22:23:25 +0900

    added carousel to cansat page

--- src/app/cansat/page.tsx
+++ src/app/cansat/page.tsx
@@ -1,5 +1,12 @@
 import HeroImage from "@/components/home/heroImage";
 import PageTitle from "@/components/pageTitle";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import Image from "next/image";
 
 export default function Page() {
@@ -18,29 +25,59 @@ export default function Page() {
         </p>
       </section>
       <section>
-        <h2 className="sm:ml-24 ml-12 sm:text-5xl text-4xl tracking-wider font-semibold mt-24">
+        <h2 className="ml-12 sm:text-5xl text-4xl tracking-wider font-semibold mt-24">
           SENSORS
         </h2>
-        <div
-          className="flex sm:flex-row flex-col justify-center w-[90%] mx-auto my-8 p-8 bg-neutral-900 rounded-xl "
-          style={{ boxShadow: "rgba(155, 155, 155, 0.8) 0 0 10px 3px" }}
-        >
-          <Image
-            src="/cansat/page/gps.jpg"
-            alt="image of gps"
-            width="400"
-            height="400"
-            className="object-cover sm:max-w-[50%]"
-          />
-          <div className="sm:w-1/2 sm:ml-6  sm:my-auto mt-6">
-            <h3 className="text-3xl tracking-wider font-semibold mb-8">
-              GPS Module
-            </h3>
-            <p className="tracking-wider leading-loose">
-              GPSモジュールはCansatに必要不可欠なセンサーです。自己位置と目的地の座標から目的地までの方角と距離を割り出します。
-            </p>
-          </div>
-        </div>
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>
+              <div
+                className="flex sm:flex-row flex-col justify-center w-[90%] mx-auto my-8 p-8 bg-black rounded-xl "
+                style={{ boxShadow: "rgba(155, 155, 155, 0.8) 0 0 7px 3px" }}
+              >
+                <Image
+                  src="/cansat/page/gps.jpg"
+                  alt="image of gps"
+                  width="400"
+                  height="400"
+                  className="object-cover sm:max-w-[50%]"
+                />
+                <div className="sm:w-1/2 sm:ml-6  sm:my-auto mt-6">
+                  <h3 className="text-3xl tracking-wider font-semibold mb-8">
+                    GPS Module
+                  </h3>
+                  <p className="tracking-wider leading-loose">
+                    GPSモジュールはCansatに必要不可欠なセンサーです。自己位置と目的地の座標から目的地までの方角と距離を割り出します。
+                  </p>
+                </div>
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div
+                className="flex sm:flex-row flex-col justify-center w-[90%] mx-auto my-8 p-8 bg-neutral-900 rounded-xl "
+                style={{ boxShadow: "rgba(155, 155, 155, 0.8) 0 0 10px 3px" }}
+              >
+                <Image
+                  src="/cansat/page/gps.jpg"
+                  alt="image of gps"
+                  width="400"
+                  height="400"
+                  className="object-cover sm:max-w-[50%]"
+                />
+                <div className="sm:w-1/2 sm:ml-6  sm:my-auto mt-6">
+                  <h3 className="text-3xl tracking-wider font-semibold mb-8">
+                    GPS Module
+                  </h3>
+                  <p className="tracking-wider leading-loose">
+                    GPSモジュールはCansatに必要不可欠なセンサーです。自己位置と目的地の座標から目的地までの方角と距離を割り出します。
+                  </p>
+                </div>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </section>
     </main>
   );
