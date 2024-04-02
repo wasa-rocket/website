bf16e17087919f45a865f6eca430db5ff6a8aff4
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-02 13:59:58 +0900

    added photo to abstract

--- src/components/carrer/abstract.tsx
+++ src/components/carrer/abstract.tsx
@@ -1,9 +1,46 @@
 import SectionTitle from "../sectionTitle";
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "../ui/carousel";
+import SlideShow from "./slideShow";
 
 export default function Abstract() {
+  const imageList = [
+    { id: 1, src: "/career/2019tanekon01.JPG", alt: "picture of wasa" },
+    { id: 2, src: "/career/2019tanekon02.JPG", alt: "picture of wasa" },
+  ];
   return (
-    <section className="my-16 mx-auto sm:w-[80%] w-[90%] max-w-[1000px] tracking-wider ">
+    <section className="my-36 mx-auto w-[80%] max-w-[1000px] tracking-wider ">
       <SectionTitle>ABOUT US</SectionTitle>
+      {/* <Carousel className="mb-16">
+        <CarouselContent>
+          <CarouselItem>
+            <Image
+              src="/career/2019tanekon01.JPG"
+              alt="picture of wasa"
+              width="600"
+              height="400"
+              className="mx-auto"
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src="/career/2019tanekon01.JPG"
+              alt="picture of wasa"
+              width="400"
+              height="400"
+            />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel> */}
+      <SlideShow imageList={imageList} />
       <p className="my-6">
         WASAロケットProject(通称・ワサロケ)は、早稲田大学理工学術院・日本女子大学公認サークルであるWASAのプロジェクトの1つで、モデルロケットやCanSatを製作し、日本各地の大会に出場するという活動を行っています。
       </p>
