0e1bb914629bf7f9986fc1f8056fb60beac89478
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-02 09:30:05 +0900

    changed faq content

--- src/components/carrer/faq.tsx
+++ src/components/carrer/faq.tsx
@@ -7,30 +7,35 @@ import {
 } from "../ui/accordion";
 
 export default function Faq() {
-  const faqContent = [
+  const faqContent: { id: number; question: string; answer: string }[] = [
     {
+      id: 1,
       question:
         "​早稲田大学・日本女子大学以外の学生もWASAロケットプロジェクトに入ることができますか？",
       answer:
         "早稲田大学・日本女子大学以外の学生の方でも大歓迎です！ただし、活動場所は主に早稲田大学戸山キャンパス・西早稲田キャンパス(新宿区)ですので、通える範囲かご確認ください。",
     },
     {
+      id: 2,
       question: "​航空や機械系以外の学科のメンバーはいますか？",
       answer:
         "理学系や文系のメンバーも所属しています。ものづくりや空が好き、興味があるという方であれば学部・学科関係なく大歓迎ですので、ぜひ新歓にお越しください！",
     },
     {
+      id: 3,
       question: "​会費はありますか？",
       answer:
         "​入会時に入会金として5000円、毎学期(春学期・秋学期)に活動費として5000円をいただいています。",
     },
     {
+      id: 4,
       question:
-        "​他のサークルやWASAのプロジェクトにも興味があるのですが、サークルを兼用することは可能でしょうか？",
+        "​他のサークルや他のWASAのプロジェクトにも興味があるのですが、兼サーすることは可能でしょうか？",
       answer:
         "はい、他のサークルにも入ることや他のWASAのプロジェクトに入ることも大丈夫です！",
     },
     {
+      id: 5,
       question:
         "​授業が忙しいと聞いたのですが、活動はどのくらいの頻度でやっているのでしょうか？",
       answer:
@@ -48,7 +53,7 @@ export default function Faq() {
       >
         {faqContent.map((faq) => {
           return (
-            <AccordionItem value={faq.question} key={faq.question}>
+            <AccordionItem value={faq.question} key={faq.id}>
               <AccordionTrigger>{faq.question}</AccordionTrigger>
               <AccordionContent>{faq.answer}</AccordionContent>
             </AccordionItem>
