8ae1d4b7ae5d6ba67ebeacc7369c7dc2933022f3
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-03 16:00:59 +0900

    dividing bento into 3 components

--- src/components/carrer/bento.tsx
+++ src/components/carrer/bento.tsx
@@ -1,72 +1,91 @@
 import Image from "next/image";
-import { BentoItemLg, BentoItemSm } from "./bentoItem";
+import {
+  BentoItemDesign,
+  BentoItemFabrication,
+  BentoItemLaunch,
+  BentoItemSm,
+} from "./bentoItem";
 
 export default function Bento() {
   return (
-    <div className="w-[80%] max-w-6xl mx-auto grid md:grid-cols-4 grid-cols-2 gap-4 relative ">
-      <BentoItemLg
-        title="設計"
-        description={
-          <div>
-            誰でも最初は初心者です
-            <br /> WASAでの経験を通じて設計を学びましょう
-          </div>
-        }
-        link="#design"
-        src="/career/TireStructure.jpg"
-        alt="design image"
-      />
+    <>
+      <div className="container lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm grid md:grid-cols-3 grid-cols-2 gap-4 relative my-12">
+        <BentoItemDesign
+          title="設計"
+          description={
+            <div>
+              誰でも最初は初心者です
+              <br /> WASAでの経験を通じて設計を学びましょう
+            </div>
+          }
+          link="#design"
+          src="/career/TireStructure.jpg"
+          alt="design image"
+        />
+        <div className="flex flex-col gap-4">
+          <BentoItemSm
+            title="Project Management"
+            src="/career/bento/project-management.png"
+            alt="project management image"
+          />
+          <BentoItemSm
+            title="CAD/Simulation"
+            src="/career/bento/3d-cube.png"
+            alt="cad image"
+          />
+        </div>
+      </div>
 
-      <BentoItemSm
-        title="Project Management"
-        src="/career/bento/project-management.png"
-        alt="project management image"
-      />
-      <BentoItemSm
-        title="CAD/Simulation"
-        src="/career/bento/3d-cube.png"
-        alt="cad image"
-      />
-      <BentoItemSm
-        title="Circuits/Programming"
-        src="/career/bento/circuit.png"
-        alt="circuit image"
-      />
-      <BentoItemSm
-        title="3D Printing"
-        src="/career/bento/3d-printing.png"
-        alt="3d-printing image"
-      />
-      <BentoItemLg
-        title="制作"
-        description={
-          <div>
-            チームで協力してアイデアを形にしよう！
-            <br />
-            ロケット制作には様々な工程があります。きっとあなたにあった作業が見つかるはず！
-          </div>
-        }
-        src="/career/modelRocket.png"
-        alt="model rocket image"
-      />
-      <BentoItemLg
-        title="打ち上げ"
-        description={
-          <div>WASAでは毎年様々な大会に出場して制作物を発表しています！</div>
-        }
-        src="/career/Noto Summer.jpg"
-        alt="launch image"
-      />
-      <BentoItemSm
-        title="3D Printing"
-        src="/career/bento/3d-printing.png"
-        alt="3d-printing image"
-      />
-      <BentoItemSm
-        title="3D Printing"
-        src="/career/bento/3d-printing.png"
-        alt="3d-printing image"
-      />
-    </div>
+      <div className="container lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm grid md:grid-cols-3 grid-cols-2 gap-4 relative my-12">
+        <div className="flex flex-col gap-4">
+          <BentoItemSm
+            title="Circuits/Programming"
+            src="/career/bento/circuit.png"
+            alt="circuit image"
+          />
+          <BentoItemSm
+            title="3D Printing"
+            src="/career/bento/3d-printing.png"
+            alt="3d-printing image"
+          />
+        </div>
+
+        <BentoItemFabrication
+          title="制作"
+          description={
+            <div>
+              チームで協力してアイデアを形にしよう！
+              <br />
+              ロケット制作には様々な工程があります。きっとあなたにあった作業が見つかるはず！
+            </div>
+          }
+          src="/career/modelRocket.png"
+          alt="model rocket image"
+        />
+      </div>
+      <div className="container lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm grid md:grid-cols-3 grid-cols-2 gap-4 relative ">
+        <BentoItemLaunch
+          title="打ち上げ"
+          description={
+            <div>WASAでは毎年様々な大会に出場して制作物を発表しています！</div>
+          }
+          src="/career/Noto Summer.jpg"
+          alt="launch image"
+          aspect="3/3"
+        />
+        <div className="flex flex-col gap-4">
+          <BentoItemSm
+            title="3D Printing"
+            src="/career/bento/3d-printing.png"
+            alt="3d-printing image"
+          />
+          <BentoItemSm
+            title="3D Printing"
+            src="/career/bento/3d-printing.png"
+            alt="3d-printing image"
+          />
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/carrer/bentoItem.tsx
+++ src/components/carrer/bentoItem.tsx
@@ -6,19 +6,71 @@ type Props = {
   description?: React.ReactNode;
   src: string;
   alt: string;
+  aspect?: string;
   link?: string;
 };
 
-export function BentoItemLg({ title, description, src, alt, link }: Props) {
+export function BentoItemDesign({
+  title,
+  description,
+  src,
+  alt,
+  link,
+  aspect = "7/5",
+}: Props) {
   return (
-    <div className="col-span-2 relative w-full md:aspect-auto aspect-[2/1] ">
+    <div className="col-span-2 relative w-full md:aspect-[7/5] text-white">
       <div className="absolute mx-4 z-50 inset-0 flex flex-col justify-center text-center">
         <div className="lg:text-5xl md:text-3xl sm:text-5xl text-2xl font-semibold lg:mb-6 mb-0">
           {title}
         </div>
         <div className="sm:text-md text-sm">{description}</div>
       </div>
-      <div className="absolute inset-0 bg-zinc-950 bg-opacity-50 z-10" />
+      <div className="absolute inset-0 bg-neutral-900 bg-opacity-50 z-10" />
+      <Image src={src} alt={alt} fill className="object-cover absolute" />
+    </div>
+  );
+}
+
+export function BentoItemFabrication({
+  title,
+  description,
+  src,
+  alt,
+  link,
+  aspect = "7/5",
+}: Props) {
+  return (
+    <div className="col-span-2 relative w-full md:aspect-[6/5] text-white">
+      <div className="absolute mx-4 z-50 inset-0 flex flex-col justify-center text-center">
+        <div className="lg:text-5xl md:text-3xl sm:text-5xl text-2xl font-semibold lg:mb-6 mb-0">
+          {title}
+        </div>
+        <div className="sm:text-md text-sm">{description}</div>
+      </div>
+      <div className="absolute inset-0 bg-neutral-900 bg-opacity-50 z-10" />
+      <Image src={src} alt={alt} fill className="object-cover absolute" />
+    </div>
+  );
+}
+
+export function BentoItemLaunch({
+  title,
+  description,
+  src,
+  alt,
+  link,
+  aspect = "7/5",
+}: Props) {
+  return (
+    <div className="col-span-2 relative w-full md:aspect-[7/5] text-white">
+      <div className="absolute mx-4 z-50 inset-0 flex flex-col justify-center text-center">
+        <div className="lg:text-5xl md:text-3xl sm:text-5xl text-2xl font-semibold lg:mb-6 mb-0">
+          {title}
+        </div>
+        <div className="sm:text-md text-sm">{description}</div>
+      </div>
+      <div className="absolute inset-0 bg-neutral-900 bg-opacity-50 z-10" />
       <Image src={src} alt={alt} fill className="object-cover absolute" />
     </div>
   );
@@ -26,9 +78,9 @@ export function BentoItemLg({ title, description, src, alt, link }: Props) {
 
 export function BentoItemSm({ title, src, alt }: Props) {
   return (
-    <div className="col-span-1 relative w-full aspect-square bg-neutral-900 px-2 pt-2 flex flex-col justify-center content-between sm:text-base text-xs">
-      <div className="h-3/4 relative">
-        <Image src={src} alt={alt} fill className="object-contain" />
+    <div className="col-span-1 relative w-full h-full bg-neutral-100 px-2 pt-2 flex flex-col justify-center sm:text-base text-xs">
+      <div className="h-1/2 relative my-auto flex items-center">
+        <Image src={src} alt={alt} layout="fill" className="object-contain" />
       </div>
       <div className="text-center my-auto">{title}</div>
     </div>
