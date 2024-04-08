87eb602e111365c0bd3c0f5e3bb0ec80904bc671
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-08 22:44:34 +0900

    modified slideshowdot.tsx

--- src/components/carrer/slideShowDot.tsx
+++ src/components/carrer/slideShowDot.tsx
@@ -4,7 +4,12 @@ type Props = {
 
 export default function SlideShowDot({ black }: Props) {
   return (
-    <svg height="10" width="10" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      height="10"
+      width="10"
+      viewBox="0 0 10 10"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <circle r="4" cx="5" cy="5" fill={black ? "black" : "#CCCCCC"} />
     </svg>
   );
