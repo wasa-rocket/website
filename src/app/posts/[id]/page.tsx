542b98f5311208b925e8935a6d859e7e4194bcfd
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-09 18:43:01 +0900

    added detailed page

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { getPostsbyId } from "@/libs/api";
+import Link from "next/link";
+import postStyles from "@/components/posts.module.css";
+
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: Params) {
+  console.log(postStyles);
+  const post = await getPostsbyId(params.id);
+  if (!post) {
+    return <h1>file not found</h1>;
+  }
+
+  return (
+    <div className="pt-36 md:max-w-screen-sm max-w-[90%] mx-auto min-h-screen">
+      <Link href="/posts" className="underline">記事一覧</Link>
+      <h1 className="text-4xl font-semibold mt-6 mb-3">{post.title}</h1>
+      <p className="text-gray-400 mb-6">{post.publishedAt}</p>
+      <div
+        className={postStyles["post"]}
+        dangerouslySetInnerHTML={{
+          __html: post.body,
+        }}
+      />
+    </div>
+  );
+}
--- src/libs/api.ts
+++ src/libs/api.ts
@@ -4,6 +4,11 @@ import { PostContent } from "@/libs/types";
 export async function getAllPosts() {
   const data = await client.get({ endpoint: "posts" });
   const contents = data.contents as PostContent[];
-  console.log(contents)
-  return contents;
+  console.log(contents);
+  return contents as PostContent[];
+}
+
+export async function getPostsbyId(id: string) {
+  const data = await client.get({ endpoint: "posts", contentId: id });
+  return data as PostContent;
 }
