67ebb7da18ce54b93a3c63cc0c6b43304df20603
Koya Saka <k7oya14@gmail.com>
2024-04-09 18:19:54 +0900

    animate Hero title

--- src/components/carrer/CareerHero.tsx
+++ src/components/carrer/CareerHero.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 function CareerHero() {
   return (
@@ -12,10 +15,26 @@ function CareerHero() {
         playsInline
       />
       <div className="absolute inset-0 flex justify-end">
-        <p className="mt-10 text-white md:text-9xl font-bold text-7xl m-5 [writing-mode:vertical-rl] flex md:flex-col flex-row md:gap-10">
-          <span className="block md:mb-auto">「意思」を、</span>
-          <span className="block md:mt-auto">打ち上げろ</span>
-        </p>
+        <div className="mt-10 text-white md:text-9xl font-bold text-7xl m-5 [writing-mode:vertical-rl] flex md:flex-col flex-row md:gap-10">
+          <div className="block md:mb-auto w-fit overflow-hidden">
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, x: [50, 0] }}
+              transition={{ duration: 0.5, delay: 0.25 }}
+            >
+              「意思」を、
+            </motion.p>
+          </div>
+          <div className="w-fit overflow-hidden block md:mt-auto">
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, x: [50, 0] }}
+              transition={{ duration: 0.5, delay: 0.25 }}
+            >
+              打ち上げろ
+            </motion.p>
+          </div>
+        </div>
       </div>
     </section>
   );
